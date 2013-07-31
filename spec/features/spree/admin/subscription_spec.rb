5dc20d42ef6a3f91863f181b7684c4958cd62e0f
Tobias Bohwalli <hi@futhr.io>
2013-07-31 22:13:21 +0200

    New feature specs with help from @AteqEjaz

--- spec/features/spree/admin/subscription_spec.rb
+++ spec/features/spree/admin/subscription_spec.rb
@@ -1,24 +1,65 @@
 require 'spec_helper'
 
-feature "Chimpy Admin", js: true do
+feature 'Chimpy Admin', js: true do
   stub_authorization!
 
-  let(:user) { create(:user) }
+  given(:user) { create(:user) }
 
-  # test so it has been add "Yes" or "No"
-
-  scenario "" do
-    # check so it has been add "Yes"
+  background do
+    visit spree.admin_path
   end
 
-  # test form
+  scenario 'new user subscription with subscription checked' do
+    click_link 'Users'
+    click_link 'New User'
 
-  scenario "" do
-    # create user
-    # access form
-    # fill in checkbox
-    # submit
-    # check flash
-    # check so user has "yes"
+    fill_in_subscription user
+
+    check 'user_subscribed'
+    click_button 'Create'
+
+    expect(current_path).to eq '/admin/users'
+    expect(page).to have_content 'API ACCESS'
+    expect(page).to have_content 'NO KEY'
+
+    find_button('Generate API key').click
+
+    fill_in_subscription user
+
+    check 'user_subscribed'
+    click_button 'Update'
+
+    expect(Spree::User.last.subscribed).to be_true
+  end
+
+  scenario 'new user subscription with subscription un-checked' do
+    click_link 'Users'
+    click_link 'New User'
+
+    fill_in_subscription user
+
+    click_button 'Create'
+
+    current_path.should eq '/admin/users'
+    expect(page).to have_content 'API ACCESS'
+    expect(page).to have_content 'NO KEY'
+
+    find_button('Generate API key').click
+
+    fill_in_subscription user
+
+    click_button 'Update'
+
+    expect(Spree::User.last.subscribed).to be_false
+  end
+
+  private
+
+  def fill_in_subscription(user)
+    expect(page).to have_content 'SUBSCRIBED TO NEWSLETTER'
+
+    fill_in 'user_email', with: "new-#{user.email}"
+    fill_in 'user_password', with: 'test123456'
+    fill_in 'user_password_confirmation', with: 'test123456'
   end
 end
--- spec/features/spree/subscription_spec.rb
+++ spec/features/spree/subscription_spec.rb
@@ -1,27 +1,43 @@
 require 'spec_helper'
 
-feature "Chimpy", js: true do
+feature 'Chimpy', js: true do
   stub_authorization!
 
-  scenario "guest subscription" do
-    # stub guest
-    # goto page
-
-    # within form
-    # fill in form
-    # submit
-    # check flash
-    # check so subscription been add
+  background do
+    visit '/signup'
   end
 
-  scenario "user subscription" do
-    # stub user
-    # goto page
+  scenario 'guest subscription deface data-hook confirmation' do
+    pending 'hook not found, what hook are we looking for here?'
+    page.find('#footer-right')
+  end
 
-    # within form
-    # fill in form
-    # submit
-    # check flash
-    # check so subscription been add
+  scenario 'user subscription with opt_in' do
+    pending 'seems to be a bug or just fails because no api key is set so its unable to deliver'
+    subscribe
+
+    expect(current_path).to eq '/'
+    expect(page).to have_selector '.notice', text: 'Welcome! You have signed up successfully.'
+    expect(Spree::User.first.subscribed).to be_true
+  end
+
+  scenario 'user subscription with opt_out' do
+    pending 'is opt_out not unsubscribe for spree_current_user?'
+    subscribe
+
+    expect(current_path).to eq '/'
+    expect(page).to have_selector '.notice', text: 'Welcome! You have signed up successfully.'
+    expect(Spree::User.first.subscribed).to be_false
+  end
+
+  def subscribe
+    expect(page).to have_text 'Sign me up to the newsletter'
+
+    fill_in 'Email', with: 'ryan@spree.com'
+    fill_in 'Password', with: 'test123456'
+    fill_in 'Password Confirmation', with: 'test123456'
+
+    check 'Sign me up to the newsletter'
+    click_button 'Create'
   end
 end
