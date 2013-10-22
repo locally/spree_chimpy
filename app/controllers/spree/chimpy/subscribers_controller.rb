d543e087c39c61def26a734881299b80bd51ad6f
dcsmith130 <dan@locally.co>
2013-10-22 10:33:31 -0400

    moved to 2.1

--- app/controllers/spree/chimpy/subscribers_controller.rb
+++ app/controllers/spree/chimpy/subscribers_controller.rb
@@ -2,8 +2,9 @@ class Spree::Chimpy::SubscribersController < ApplicationController
   respond_to :html
 
   def create
-    @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
-    @subscriber.update_attributes(params[:chimpy_subscriber])
+    @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first
+    @subscriber = Spree::Chimpy::Subscriber.new(subscriber_params) unless @subscriber
+    @subscriber.update_attributes(subscriber_params)
     if @subscriber.save
       Spree::Chimpy::Subscription.new(@subscriber).subscribe
       flash[:notice] = Spree.t(:success, scope: [:chimpy, :subscriber])
@@ -14,3 +15,11 @@ class Spree::Chimpy::SubscribersController < ApplicationController
     respond_with @subscriber, location: request.referer
   end
 end
+
+  def permitted_subscriber_attributes
+    [:email]
+  end
+private
+  def subscriber_params
+    params.require(:chimpy_subscriber).permit(permitted_subscriber_attributes)
+  end
